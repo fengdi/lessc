898494fe2610088c6795f89817cc77761efbdc1f
tangoboy <zhulutangoboy@gmail.com>
2013-01-15 19:43:58 +0800

    fixed #1
    解决中文路径问题，更新less.js为1.3.3

---
+++ README.md
@@ -0,0 +1,12 @@
+# LESS compiler for Sublime Text 2
+
+  - 利用windows自带js引擎解析less文件(*仅支持windows*)
+  - 保存less时，自动重新读取css
+  - 可设置是否压缩css
+  - 保持Sublime轻巧简洁
+
+**将代码下载到 "Sublime Text 2\Data\Packages\lessc"**
+
+[下载]代码
+
+[下载]: https://github.com/fengdi/lessc
--- lessc.py
+++ lessc.py
@@ -1,44 +1,47 @@
+# coding=utf-8
 # LESS compiler for Sublime Text 2 in windows
 # http://www.cnblogs.com/tangoboy/
 # Licensed under the WTFPL
 
-import os, sys, subprocess, functools, sublime, sublime_plugin
+import os, sys, re, subprocess, functools, sublime, sublime_plugin
 
 package_name = 'lessc'
 
+#print sys.getdefaultencoding()
+#vars()
 
 def lessc(fileroot):
+  
+	is_compress = sublime.load_settings('lessc.sublime-settings').get('compress') #获取是否压缩的配置
 
-	is_compress = sublime.load_settings('lessc.sublime-settings').get('compress')
-
-	#execmd = '"'+sublime.packages_path()+'\\'+package_name+'\\lessc.cmd"'+' "'+fileroot+'.less"'+' "'+fileroot+'.css"'
 	execmd = '@cscript //nologo "'+sublime.packages_path()+'\\'+package_name+'\\lessc.wsf"'+' "'+fileroot+'.less"'+' "'+fileroot+'.css"'
 	
 	if is_compress:
 		execmd += ' -compress'
 
+	execmd = execmd.encode("gbk") #先将编码转换到gbk
+
 	res = subprocess.Popen(execmd,stdin = subprocess.PIPE,stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
 	res.wait()
 
 	error = res.stderr.read()
-	print error
+	print error.decode("gbk")  #解码gbk
 	
 	remsg = '';
 	if error=='':
 		remsg = ' ** compild:'+fileroot+'.css ** '
 	else:
 		errorinfo = error.split("\r\n")
-		#print errorinfo
 		remsg = errorinfo[2]+"    "+errorinfo[6]+""
-		#remsg = 'Error:'+re.match('message\:,*',error).groups()
 
 	sublime.set_timeout(functools.partial(status,remsg),1200);
 	sublime.set_timeout(functools.partial(reloadCss,fileroot),400);
 
-
+#状态栏消息
 def status(msg):
 	sublime.status_message(msg)
 
+#重新读取css文件
 def reloadCss(fileroot):
 	for win in sublime.windows():
 		for view in win.views():
